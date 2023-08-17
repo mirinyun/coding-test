a6f1e1c1609fb123930e3381d410cc51f338516d
mirinyun <132061904+mirinyun@users.noreply.github.com>
2023-08-17 17:12:53 +0900

    Update test_greedy.py

--- .venv/tests/test_greedy.py
+++ .venv/tests/test_greedy.py
@@ -1,11 +1,10 @@
 import unittest
 
 from src.greedy import (coin_split,
-                        law_of_large_numbers,
-                        count_with_three_in_time,
+                        law_of_large_numbers, 
+                        count_with_three_in_time, 
                         k_knight,
                         find_prime)
-
 
 class TestGreedy(unittest.TestCase):
 
@@ -14,9 +13,9 @@ class TestGreedy(unittest.TestCase):
         self.assertEqual(coin_split(total_value=660), 4)
 
     def test_law_of_large_numbers(self):
-        answer1 = law_of_large_numbers((5, 8, 3), [2, 4, 5, 4, 6])
+        answer1 = law_of_large_numbers((5,8,3), [2,4,5,4,6])
         self.assertEqual(answer1, 46)
-        answer2 = law_of_large_numbers((5, 3, 3), [2, 4, 5, 4, 6])
+        answer2 = law_of_large_numbers((5,3,3), [2,4,5,4,6])
         self.assertEqual(answer2, 18)
 
     def test_count_with_three_in_time(self):
@@ -24,6 +23,7 @@ class TestGreedy(unittest.TestCase):
 
     def test_k_knight(self):
         self.assertEqual(k_knight("a1"), 2)
-
-    def test_find_prime(self):
+    
+    def test_find_prims(self):
         self.assertEqual(find_prime("17"), 3)
+        self.assertEqual(find_prime("011"), 2)        
